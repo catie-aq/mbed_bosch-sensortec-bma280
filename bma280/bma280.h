715a5a7e8a98d1ed0d133f6f041324c94562d597
Pierre-Marie Ancele <pm.ancele@catie.fr>
2017-07-03 12:25:48 +0200

    Add resgister definitions

--- bma280/bma280.h
+++ bma280/bma280.h
@@ -19,10 +19,80 @@
 
 namespace sixtron {
 
-class LibTemplate
+class BMA280
 {
 public:
-    LibTemplate();
+    enum class I2CAddress {
+        Address1 = 0x18,
+        Address2 = 0x19
+    };
+
+    enum class RegisterAddress : char {
+    ChipId						=    (0x00),
+    /** DATA ADDRESS DEFINITIONS */
+    X_Axis_Lsb                  =    (0x02),
+    X_Axis_Msb                  =    (0x03),
+    Y_Axis_Lsb                  =    (0x04),
+    Y_Axis_Msb                  =    (0x05),
+    Z_Axis_Lsb                  =    (0x06),
+    Z_Axis_Msb                  =    (0x07),
+    Temp						=    (0x08),
+    /**STATUS ADDRESS DEFINITIONS */
+    Stat1						=    (0x09),
+    Stat2						=    (0x0A),
+    StatTapSlop					=    (0x0B),
+    StatOrientHigh				=    (0x0C),
+    StatFifo					=    (0x0E),
+    /**STATUS ADDRESS DEFINITIONS */
+    RangeSelect					=    (0x0F),
+    BwSelect   	                =    (0x10),
+    ModeCtrl                    =    (0x11),
+    LowNoiseCtrl	            =    (0x12),
+    DataCtrl                    =    (0x13),
+    Rst                         =    (0x14),
+    /**INTERUPT ADDRESS DEFINITIONS */
+    IntrEnable1                 =    (0x16),
+    IntrEnable2                 =    (0x17),
+    IntrSlowNoMotion	        =    (0x18),
+    Intr1PadSelect              =    (0x19),
+    IntrDataSelect              =    (0x1A),
+	Intr2PadSelect              =    (0x1B),
+    IntrSource                  =    (0x1E),
+    IntrSet                     =    (0x20),
+    IntrCtrl                    =    (0x21),
+    /** FEATURE ADDRESS DEFINITIONS */
+    Low_Durn                    =    (0x22),
+    Low_Thres                   =    (0x23),
+    Low_High_Hyst               =    (0x24),
+    High_Durn                   =    (0x25),
+    High_Thres                  =    (0x26),
+    Slope_Durn                  =    (0x27),
+    Slope_Thres                 =    (0x28),
+    SlowNoMotionThres           =    (0x29),
+    TapParam                    =    (0x2A),
+    TapThres                    =    (0x2B),
+    OrientParam                 =    (0x2C),
+    ThetaBlock                  =    (0x2D),
+    ThetaFlat                   =    (0x2E),
+    FlatHoldTime                =    (0x2F),
+    SelfTest                    =    (0x32),
+    EepromCtrl                  =    (0x33),
+    SerialCtrl                  =    (0x34),
+    /**OFFSET ADDRESS DEFINITIONS */
+    OffsetCtrl                  =    (0x36),
+    OffsetParams                =    (0x37),
+    Offset_X_Axis               =    (0x38),
+    Offset_Y_Axis               =    (0x39),
+    Offset_Z_Axis               =    (0x3A),
+    /**GP ADDRESS DEFINITIONS */
+    Gp0                         =    (0x3B),
+    Gp1                         =    (0x3C),
+    /**FIFO ADDRESS DEFINITIONS */
+    FifoMode                    =    (0x3E),
+    FifoDataOutput              =    (0x3F),
+    FifoWmlTrig                 =    (0x30),
+    };
+
 };
 
 } // namespace sixtron
