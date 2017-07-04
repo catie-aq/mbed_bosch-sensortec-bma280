63d803a707ac1fca768d5a92b7910e714995fbc0
Pierre-Marie Ancele <pm.ancele@catie.fr>
2017-07-04 17:25:49 +0200

    WIP

--- bma280/bma280.h
+++ bma280/bma280.h
@@ -17,7 +17,17 @@
 #ifndef CATIE_SIXTRON_LIBTEMPLATE_H_
 #define CATIE_SIXTRON_LIBTEMPLATE_H_
 
+#include "mbed.h"
+
 namespace sixtron {
+
+/* accel values in m/s^2 */
+typedef struct
+{
+    double x;
+    double y;
+    double z;
+} bma280_accel_t;
 
 class BMA280
 {
@@ -28,72 +38,111 @@ public:
     };
 
     enum class RegisterAddress : char {
-    ChipId						=    (0x00),
-    /** DATA ADDRESS DEFINITIONS */
-    X_Axis_Lsb                  =    (0x02),
-    X_Axis_Msb                  =    (0x03),
-    Y_Axis_Lsb                  =    (0x04),
-    Y_Axis_Msb                  =    (0x05),
-    Z_Axis_Lsb                  =    (0x06),
-    Z_Axis_Msb                  =    (0x07),
-    Temp						=    (0x08),
-    /**STATUS ADDRESS DEFINITIONS */
-    Stat1						=    (0x09),
-    Stat2						=    (0x0A),
-    StatTapSlop					=    (0x0B),
-    StatOrientHigh				=    (0x0C),
-    StatFifo					=    (0x0E),
-    /**STATUS ADDRESS DEFINITIONS */
-    RangeSelect					=    (0x0F),
-    BwSelect   	                =    (0x10),
-    ModeCtrl                    =    (0x11),
-    LowNoiseCtrl	            =    (0x12),
-    DataCtrl                    =    (0x13),
-    Rst                         =    (0x14),
-    /**INTERUPT ADDRESS DEFINITIONS */
-    IntrEnable1                 =    (0x16),
-    IntrEnable2                 =    (0x17),
-    IntrSlowNoMotion	        =    (0x18),
-    Intr1PadSelect              =    (0x19),
-    IntrDataSelect              =    (0x1A),
-	Intr2PadSelect              =    (0x1B),
-    IntrSource                  =    (0x1E),
-    IntrSet                     =    (0x20),
-    IntrCtrl                    =    (0x21),
-    /** FEATURE ADDRESS DEFINITIONS */
-    Low_Durn                    =    (0x22),
-    Low_Thres                   =    (0x23),
-    Low_High_Hyst               =    (0x24),
-    High_Durn                   =    (0x25),
-    High_Thres                  =    (0x26),
-    Slope_Durn                  =    (0x27),
-    Slope_Thres                 =    (0x28),
-    SlowNoMotionThres           =    (0x29),
-    TapParam                    =    (0x2A),
-    TapThres                    =    (0x2B),
-    OrientParam                 =    (0x2C),
-    ThetaBlock                  =    (0x2D),
-    ThetaFlat                   =    (0x2E),
-    FlatHoldTime                =    (0x2F),
-    SelfTest                    =    (0x32),
-    EepromCtrl                  =    (0x33),
-    SerialCtrl                  =    (0x34),
-    /**OFFSET ADDRESS DEFINITIONS */
-    OffsetCtrl                  =    (0x36),
-    OffsetParams                =    (0x37),
-    Offset_X_Axis               =    (0x38),
-    Offset_Y_Axis               =    (0x39),
-    Offset_Z_Axis               =    (0x3A),
-    /**GP ADDRESS DEFINITIONS */
-    Gp0                         =    (0x3B),
-    Gp1                         =    (0x3C),
-    /**FIFO ADDRESS DEFINITIONS */
-    FifoMode                    =    (0x3E),
-    FifoDataOutput              =    (0x3F),
-    FifoWmlTrig                 =    (0x30),
+		ChipId						=    (0x00),
+		/** DATA ADDRESS DEFINITIONS */
+		X_Axis_Lsb                  =    (0x02),
+		X_Axis_Msb                  =    (0x03),
+		Y_Axis_Lsb                  =    (0x04),
+		Y_Axis_Msb                  =    (0x05),
+		Z_Axis_Lsb                  =    (0x06),
+		Z_Axis_Msb                  =    (0x07),
+		Temp						=    (0x08),
+		/**STATUS ADDRESS DEFINITIONS */
+		Stat1						=    (0x09),
+		Stat2						=    (0x0A),
+		StatTapSlop					=    (0x0B),
+		StatOrientHigh				=    (0x0C),
+		StatFifo					=    (0x0E),
+		/**STATUS ADDRESS DEFINITIONS */
+		RangeSelect					=    (0x0F),
+		BwSelect   	                =    (0x10),
+		ModeCtrl                    =    (0x11),
+		LowNoiseCtrl	            =    (0x12),
+		DataCtrl                    =    (0x13),
+		Rst                         =    (0x14),
+		/**INTERUPT ADDRESS DEFINITIONS */
+		IntrEnable1                 =    (0x16),
+		IntrEnable2                 =    (0x17),
+		IntrSlowNoMotion	        =    (0x18),
+		Intr1PadSelect              =    (0x19),
+		IntrDataSelect              =    (0x1A),
+		Intr2PadSelect              =    (0x1B),
+		IntrSource                  =    (0x1E),
+		IntrSet                     =    (0x20),
+		IntrCtrl                    =    (0x21),
+		/** FEATURE ADDRESS DEFINITIONS */
+		Low_Durn                    =    (0x22),
+		Low_Thres                   =    (0x23),
+		Low_High_Hyst               =    (0x24),
+		High_Durn                   =    (0x25),
+		High_Thres                  =    (0x26),
+		Slope_Durn                  =    (0x27),
+		Slope_Thres                 =    (0x28),
+		SlowNoMotionThres           =    (0x29),
+		TapParam                    =    (0x2A),
+		TapThres                    =    (0x2B),
+		OrientParam                 =    (0x2C),
+		ThetaBlock                  =    (0x2D),
+		ThetaFlat                   =    (0x2E),
+		FlatHoldTime                =    (0x2F),
+		SelfTest                    =    (0x32),
+		EepromCtrl                  =    (0x33),
+		SerialCtrl                  =    (0x34),
+		/**OFFSET ADDRESS DEFINITIONS */
+		OffsetCtrl                  =    (0x36),
+		OffsetParams                =    (0x37),
+		Offset_X_Axis               =    (0x38),
+		Offset_Y_Axis               =    (0x39),
+		Offset_Z_Axis               =    (0x3A),
+		/**GP ADDRESS DEFINITIONS */
+		Gp0                         =    (0x3B),
+		Gp1                         =    (0x3C),
+		/**FIFO ADDRESS DEFINITIONS */
+		FifoMode                    =    (0x3E),
+		FifoDataOutput              =    (0x3F),
+		FifoWmlTrig                 =    (0x30),
     };
 
+    enum class PowerMode: char {
+    	PowerMode_NORMAL		=(0X00),
+    	PowerMode_DEEP_SUSPEND	=(0X01),
+		PowerMode_LOW_POWER		=(0X02),
+		PowerMode_SUSPEND		=(0x04)
+    };
+
+    enum class Range: char {
+    	Range_2g		=(0X03),
+    	Range_4g		=(0X05),
+		Range_8g		=(0X08),
+		Range_16g		=(0x0C)
+    };
+
+    BMA280(I2C * i2c, I2CAddress address = I2CAddress::Address1, int hz = 400000);
+
+    bool initialize();
+    void set_power_mode(PowerMode mode);
+    // TODO configure power mode, sleep duration etc
+    // TODO configure bandwidth range
+    // TODO bool update() when data are ready ?
+
+
+    void read_accel(bma280_accel_t* accel);
+    void read_temperature(int8_t* temperature);
+
+    char chip_id() { return _chipId; }
+
+private:
+
+    int i2c_set_register(RegisterAddress registerAddress, char value);
+    int i2c_read_register(RegisterAddress registerAddress, char *value);
+    int i2c_read_vector(RegisterAddress registerAddress, int16_t value[3]);
+
+    I2C* _i2c;
+    I2CAddress _i2cAddress;
+    Range _range;
+    char _chipId = 0;
 };
+
 
 } // namespace sixtron
 
--- source/bma280.cpp
+++ source/bma280.cpp
@@ -15,12 +15,114 @@
  * limitations under the License.
  */
 
-#include "libtemplate/libtemplate.h"
+#include "bma280.h"
 
 namespace sixtron {
 
-LibTemplate::LibTemplate()
+BMA280::BMA280(I2C * i2c, I2CAddress address, int hz):
+		_i2cAddress(address), _range(Range::Range_2g)
 {
+	_i2c = i2c;
+    _i2c->frequency(hz);
+}
+
+bool BMA280::initialize()
+{
+	char reg = 0;
+	printf("Initializing BMA280 ... \n");
+	i2c_read_register(RegisterAddress::ChipId, &reg);
+	if (reg != 0XFB) {
+		wait_ms(1000); //BNO055 may have not finishing to boot !
+		i2c_read_register(RegisterAddress::ChipId, &reg);
+		if (reg != 0XFB) {
+			return false;
+		}
+	}
+	_chipId = reg;
+	return true;
+}
+
+void BMA280::read_accel(bma280_accel_t* accel)
+{
+	static int16_t acc[3];
+	i2c_read_vector(RegisterAddress::X_Axis_Lsb, acc);
+
+	accel->x = static_cast<double>((acc[0] >> 2)/100.0); //The 2 first bits are not data
+	accel->y = static_cast<double>((acc[1] >> 2)/100.0); //The 2 first bits are not data
+	accel->z = static_cast<double>((acc[2] >> 2)/100.0); //The 2 first bits are not data
+}
+
+void BMA280::set_power_mode(PowerMode mode)
+{
+	uint8_t data = (static_cast<char>(mode) << 5);
+    i2c_set_register(RegisterAddress::ModeCtrl, data);
+}
+
+/** Set register value
+ *
+ * @param registerAddress register address
+ * @param value value to write
+ *
+ * @returns
+ *      O on success,
+ *      non-0 on failure
+ */
+int BMA280::i2c_set_register(RegisterAddress registerAddress, char value)
+{
+    static char data[2];
+    data[0] = static_cast<char>(registerAddress);
+    data[1] = value;
+    if (_i2c->write(static_cast<int>(_i2cAddress) << 1, data, 2, false) != 0) {
+        return -1;
+    }
+    return 0;
+}
+
+/** Get register value
+ *
+ * @param registerAddress register address
+ * @param value pointer to store read value to
+ *
+ * @returns
+ *      O on success,
+ *      non-0 on failure
+ */
+int BMA280::i2c_read_register(RegisterAddress registerAddress, char *value)
+{
+    char data = static_cast<char>(registerAddress);
+    if (_i2c->write(static_cast<int>(_i2cAddress) << 1, &data, 1, true) != 0) {
+        return -1;
+    }
+    if (_i2c->read(static_cast<int>(_i2cAddress) << 1, value, 1, false) != 0) {
+        return -2;
+    }
+    return 0;
+}
+
+/** Get multi-byte register value (3*2-bytes) that are stored in a 3 dimensions vector
+ *
+ * @param registerAddress register address of LSB
+ * @param value pointer to store read value to
+ *
+ * @returns
+ *      O on success,
+ *      non-0 on failure
+ */
+int BMA280::i2c_read_vector(RegisterAddress registerAddress, int16_t value[3])
+{
+    static char data[6];
+    data[0] = static_cast<char>(registerAddress);
+    if (_i2c->write(static_cast<int>(_i2cAddress) << 1, data, 1, true) != 0) {
+        return -1;
+    }
+    if (_i2c->read(static_cast<int>(_i2cAddress) << 1, data, 6, false) != 0) {
+        return -2;
+    }
+    value[0] = (data[1] << 8) | (0xFF & data[0]);
+    value[1] = (data[3] << 8) | (0xFF & data[2]);
+    value[2] = (data[5] << 8) | (0xFF & data[4]);
+
+    return 0;
 }
 
 } // namespace sixtron
